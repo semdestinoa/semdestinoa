d9fa9cda7c54d78995f042ac32c5aa717dbfc807
guilhermesoeiro <guilhermesoeiro.art@gmail.com>
2017-09-12 16:16:17 +0100

    expo_this

--- js/main.js
+++ js/main.js
@@ -101,9 +101,33 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
+};
+var slideIndex2 = 1;
+showSlides2(slideIndex2);
+
+function plusSlides2(n) {
+  showSlides2(slideIndex2 += n);
 }
 
+function currentSlide2(n) {
+  showSlides2(slideIndex2 = n);
+}
 
+function showSlides2(n) {
+  var ib;
+  var slides2 = document.getElementsByClassName("mySlides2");
+  var dots2 = document.getElementsByClassName("dot2");
+  if (n > slides2.length) {slideIndex2 = 1}    
+  if (n < 1) {slideIndex2 = slides2.length}
+  for (i = 0; i < slides2.length; i++) {
+      slides2[i].style.display = "none";  
+  }
+  for (i = 0; i < dots2.length; i++) {
+      dots2[i].className = dots2[i].className.replace(" active", "");
+  }
+  slides2[slideIndex2-1].style.display = "block";  
+  dots2[slideIndex2-1].className += " active";
+};
 /*jQuery( document ).ready(function() {
 	
 	jQuery(".sidemenu .ap").click(function() {
